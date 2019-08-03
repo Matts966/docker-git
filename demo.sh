ab549971488e883b44879831171c409ebcd65a1c
Matts966 <Matts966@users.noreply.github.com>
2019-08-03 20:21:13 +0900

    Checkout a commit to make the REPL work

--- demo.sh
+++ demo.sh
@@ -4,5 +4,6 @@ cd `dirname $0`
 docker run -it --rm -v /var/run/docker.sock:/var/run/docker.sock \
     docker-git /bin/sh -c "git clone https://github.com/vlang/v; \
         cd v; \
+        git checkout 7918a790b4a1792c67d9ca04fab3425f1454eab9; \
         docker build -t vlang . ; \
         docker run --rm -it vlang:latest v;"
